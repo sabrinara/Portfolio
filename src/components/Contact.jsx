b5458a36c5c0d3a9689c0a5ebfdf0e366b139e7a
sabrinara <saashid18@gmail.com>
2024-04-21 15:30:01 +0600

    some changes inthe contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
     const sendEmail = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('service_owqdnfj', 'template_85v27r1', form.current, 'l1mlj_HxUNVJsmvJJ')
+        emailjs.sendForm('service_x01nv7f', 'template_k2yu6ss', form.current, 'l1mlj_HxUNVJsmvJJ')
             .then((result) => {
                 if (result.text === 'OK') {
                     toast.success('Message send successfully');
@@ -23,24 +23,25 @@ const Contact = () => {
     return (
         <div name="contact" className='pt-20'>
             <Title title="Contact" ></Title>
-            <div className="px-4 pb-4 md:px-16 lg:px-8 lg:pb-8 mx-auto mb-10 pt-10"  data-aos="fade-right" data-aos-duration="1000">
+            <div className="px-4 pb-4 md:px-16 lg:pb-8 mx-auto mb-10 "  data-aos="fade-right" data-aos-duration="1000">
             <div className="flex flex-col justify-center mx-auto h-full">
                 <div className="pb-8">
                     <p className="text-sky-400 mt-5 text-2xl leading-relaxed text-center font-semibold opacity-80">Submit the form below to get in touch with me!</p>
                 </div>
                 <div className=" flex justify-center items-center">
                     <form ref={form} onSubmit={sendEmail} className="flex flex-col w-full md:w-1/2 gap-4">
+                       
                         <input
                             required
                             type="text"
-                            name="to_name"
+                            name="user_name"
                             placeholder="Enter your name"
                             className="rounded border text-white border-sky-400 bg-slate-950 py-3 px-3 text-xl leading-8 outline-none transition-colors duration-200 ease-in-out "
                         />
                         <input
                             required
                             type="text"
-                            name="from_email"
+                            name="user_email"
                             placeholder="Enter your email"
                             className="rounded border text-white border-sky-400 bg-slate-950 py-3 px-3 text-xl leading-8 outline-none transition-colors duration-200 ease-in-out "
                         />
@@ -53,7 +54,7 @@ const Contact = () => {
                         ></textarea>
 
                         <div className='flex w-full justify-end p-2'>
-                            <button className="text-md mb-2 inline-flex w-28 items-center justify-center rounded-lg border bg-primary-700 px-1 py-2.5 font-medium text-sky hover:bg-primary-700/80 focus:outline-none focus:ring-2 dark:border-primary-700 dark:bg-primary-500 dark:hover:bg-primary-700 dark:focus:ring-primary-600">
+                            <button value="Send" type="submit" className="text-md mb-2 inline-flex w-28 items-center justify-center rounded-lg border bg-primary-700 px-1 py-2.5 font-medium text-sky hover:bg-primary-700/80 focus:outline-none focus:ring-2 dark:border-primary-700 dark:bg-primary-500 dark:hover:bg-primary-700 dark:focus:ring-primary-600">
                                 Send <IoMdSend className="ml-2"></IoMdSend>
                             </button>
                         </div>
