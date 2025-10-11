46752b3bbe6aad163e4818313a14e9f2ee4396f6
Sabrina Rashid <54745113+sabrinara@users.noreply.github.com>
2025-10-12 02:44:50 +0600

    color selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,47 @@
 import Image from "next/image";
 import { ModeToggle } from "./shared/ModeToggle";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Home() {
   return (
-  <div className="bg-background text-foreground p-6 rounded-lg">
-  <h1 className="text-primary text-2xl font-bold">Hello Shadcn + Tailwind v4</h1>
-  <p className="text-secondary">Custom color palette working perfectly!</p>
-  <button className="bg-accent text-background px-4 py-2 rounded mt-4">
-    Click Me
-  </button>
-  <ModeToggle/>
-</div>
+    <div className="bg-background text-foreground p-6 rounded-lg">
+      <h1 className="text-primary text-2xl font-bold">Hello Shadcn + Tailwind v4</h1>
+      <p className="">Custom color palette working perfectly!</p>
+      <button className="bg-accent text-buttontext px-4 py-2 rounded mt-4">
+        Click Me
+      </button>
+      <br />
+      <br />
+      <Button variant="secondary" className="text-buttontext" >
+        Click Me
+      </Button>
+<ModeToggle />
+      <br />
+      <br />
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardAction>Card Action</CardAction>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      
+    </div>
 
   );
 }
